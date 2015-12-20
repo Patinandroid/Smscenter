31b7a745f0e6febbfe0f44dfc07eae51e9a9f708
patinandroid <patina@dbmail.dk>
2015-12-21 00:56:46 +0100

    Nu med ipadressedetektor

--- src/IP.java
+++ src/IP.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
+ * Hent ipadresse ved hjælp af ifconfig og lidt regex-trylleri.
  * Created by tomas on 12/20/15.
  */
 public class IP {
-   String shellCommand = "/sbin/ifconfig eth0";
+	String eth = "eth0";
+   String shellCommand = "/sbin/ifconfig " + eth;
    Scanner sc;
 
    public String getIpAddress(){
-      String ipaddress = "FAIL!";
+      String ipaddress = "Failed to get ipaddress of " + eth;
       try{
          Runtime r = Runtime.getRuntime();
          Process p = r.exec(shellCommand);
@@ -21,11 +23,10 @@ public class IP {
          sc = new Scanner(stdin);
          Pattern pattern = Pattern.compile("\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b");
 
-         while( (ipaddress = sc.findInLine(pattern)) == null) {
-            sc.nextLine();
-         }
-      }catch(IOException e){System.out.println("IOException in " + this.getClass());}
-      //System.out.println(ipaddress); // DEBUG
+			while( sc.hasNextLine() && ((ipaddress = sc.findInLine(pattern))== null) ){
+				sc.nextLine();
+			}
+      }catch(IOException e){System.out.println("IOException: "+e+"     in " + this.getClass());}
       return ipaddress;
    }
 }
